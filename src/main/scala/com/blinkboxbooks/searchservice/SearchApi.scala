24bc744da5ff9ff221324b5ac504bb21f2bcf382
Jan Stette <jans@blinkbox.com>
2014-04-28 18:51:10 +0100

    Separated different part of routes, and tidied up routes further

--- src/main/scala/com/blinkboxbooks/searchservice/SearchApi.scala
+++ src/main/scala/com/blinkboxbooks/searchservice/SearchApi.scala
@@ -12,11 +12,25 @@ import spray.httpx.Json4sJacksonSupport
 import spray.routing.HttpService
 import spray.routing.HttpServiceActor
 import com.blinkboxbooks.common.spray.BlinkboxHelpers
+import spray.routing.Route
+
+/**
+ * Abstract defintions of REST API for search service.
+ */
+trait SearchRoutes {
+
+  def searchForBooks: Route
+
+  def similarBooks: Route
+
+  def searchSuggestions: Route
+
+}
 
 /**
  * API for search service, expressed as Spray routes.
  */
-trait SearchApi extends HttpService with Json4sJacksonSupport with BlinkboxHelpers {
+trait SearchApi extends HttpService with SearchRoutes with Json4sJacksonSupport with BlinkboxHelpers {
 
   // Abstract values, to be provided by concrete implementations.
   val model: SearchModel
@@ -36,43 +50,50 @@ trait SearchApi extends HttpService with Json4sJacksonSupport with BlinkboxHelpe
     books: List[Book],
     links: Seq[PageLink])
 
+  override val searchForBooks =
+    pathSuffix("books") {
+      paged(defaultCount = 50) { page =>
+        parameters('q, 'order ?, 'desc.as[Boolean] ? true) { (query, order, desc) =>
+          val result = model.search(query, page.offset, page.count, order, desc)
+          onSuccess(result) { foundBooks =>
+            complete(SearchResult("urn:blinkboxbooks:schema:search",
+              query, foundBooks.size, foundBooks,
+              links(foundBooks.size, page.offset, page.count, s"$baseUrl/books")))
+          }
+        }
+      }
+    }
+
+  override val similarBooks =
+    path("books" / Segment / "similar") { id =>
+      paged(defaultCount = 10) { page =>
+        val result = model.findSimilar(id, page.offset, page.count)
+        onSuccess(result) { foundBooks =>
+          complete(SearchResult("urn:blinkboxbooks:schema:search:similar",
+            id, foundBooks.size, foundBooks,
+            links(foundBooks.size, page.offset, page.count, s"$baseUrl/books/$id/similar")))
+        }
+      }
+    }
+
+  override val searchSuggestions =
+    path("suggestions") {
+      paged(defaultCount = 50) { page =>
+        parameters('q) { query =>
+          val result = model.suggestions(query, page.offset, page.count)
+          onSuccess(result) { foundBooks =>
+            complete(SuggestionsResult("urn:blinkboxbooks:schema:list", foundBooks))
+          }
+        }
+      }
+    }
+
   val route = handleRejections(invalidParamHandler) {
     addBBBMediaTypeToResponse {
       get {
-        pathPrefix("search" / "books") {
-          pathEnd {
-            paged(defaultCount = 50) { page =>
-              parameters('q, 'order ?, 'desc.as[Boolean] ? true) { (query, order, desc) =>
-                val result = model.search(query, page.offset, page.count, order, desc)
-                onSuccess(result) { foundBooks =>
-                  complete(SearchResult("urn:blinkboxbooks:schema:search",
-                    query, foundBooks.size, foundBooks,
-                    links(foundBooks.size, page.offset, page.count, s"$baseUrl/books")))
-                }
-              }
-            }
-          } ~
-            path(Segment / "similar") { id =>
-              paged(defaultCount = 10) { page =>
-                val result = model.findSimilar(id, page.offset, page.count)
-                onSuccess(result) { foundBooks =>
-                  complete(SearchResult("urn:blinkboxbooks:schema:search:similar",
-                    id, foundBooks.size, foundBooks,
-                    links(foundBooks.size, page.offset, page.count, s"$baseUrl/books/$id/similar")))
-                }
-              }
-            }
-        } ~
-          path("search" / "suggestions") {
-            paged(defaultCount = 50) { page =>
-              parameters('q) { query =>
-                val result = model.suggestions(query, page.offset, page.count)
-                onSuccess(result) { foundBooks =>
-                  complete(SuggestionsResult("urn:blinkboxbooks:schema:list", foundBooks))
-                }
-              }
-            }
-          }
+        pathPrefix("search") {
+          searchForBooks ~ similarBooks ~ searchSuggestions
+        }
       }
     }
   }
